3cc742f5ab0fa488e886a776faf0736aece9f93b
Kate Maltseva <kotoyama@list.ru>
2019-07-10 20:47:52 +0500

    test

--- imports/twitter_import.py
+++ imports/twitter_import.py
@@ -1,26 +1,34 @@
 import requests
-import datetime
+import urllib
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
-
 from imports.import_base import ImportBase
 from common.feed_element import FeedElement
 from common.downloaded_image import DownloadedImage
 
+
 class TwitterImport(ImportBase):
-	def get_key(self):
-		return "twitter"
+    def get_key(self):
+        return 'twitter'
 
-	def get_elements(self, count):
-		url = "https://twitter.com/elonmusk"
-		html = urlopen(url)
-		soup = BeautifulSoup(html, 'html.parser')
-		tweets = soup.find_all('li', { "data-item-type":"tweet" })
-		result = FeedElement()
+    def get_elements(self, count):
+        url = 'https://twitter.com/elonmusk'
+        html = urlopen(url)
+        soup = BeautifulSoup(html, 'html.parser')
+        result = []
 
-		for t in tweets:
-			result.author = "Elon Musk"
-			result.body = t.p.get_text().replace('\n', '')
-			result.date = t.small.a['title']
-		return [ result ]
+        tweets = soup.find_all('link', {'data-item-type': 'tweet'})
+
+        for t in tweets:
+            element = FeedElement()
+            element.author = t.find('span', {'class': 'username'}).get_text()
+            element.body = t.find('p', {"class": 'tweet-text'}).get_text()
+            element.date = t.find(
+                'a', {'class': 'tweet-timestamp'}).attrs['title']
+
+            result.append(element)
+            print(len(result))
+            # if len(result) >= count:
+            #     break
+        return result
--- main.py
+++ main.py
@@ -8,25 +8,25 @@ from imports.twitter_import import TwitterImport
 from exports.stupid_html_export import StupidHtmlExport
 from storage.inmemory_storage import InmemoryStorage
 
-imports = [ TwitterImport() ]
-exports = [ StupidHtmlExport("export.html") ]
+imports = [TwitterImport()]
+exports = [StupidHtmlExport("export.html")]
 storage = InmemoryStorage()
 
 while True:
-	for i in imports:
-		result = i.get_elements(30)
+    for i in imports:
+        result = i.get_elements(30)
 
-		for r in result:
-			print(r.date, r.author)
-			print(r.body)
-			print(r.images)
-			print(r.videos)
-			print()
-			storage.add_element(i.get_key(), r)
+        for r in result:
+            print(r.date, r.author)
+            print(r.body)
+            print(r.images)
+            print(r.videos)
+            print()
+            storage.add_element(i.get_key(), r)
 
-			# TODO: add check for duplicated items somewhere (most likely as filter request to storage)
-			for e in exports:
-				e.export(r)
+            # TODO: add check for duplicated items somewhere (most likely as filter request to storage)
+            for e in exports:
+                e.export(r)
 
-	# sleep for 30 seconds to avoid flood with mock stuff
-	time.sleep(30)
+    # sleep for 30 seconds to avoid flood with mock stuff
+    time.sleep(30)
