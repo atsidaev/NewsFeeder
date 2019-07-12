4fd82c5798ee5f43500873352de07022d9e7ae4c
Kate Maltseva <kotoyama@list.ru>
2019-07-12 21:52:04 +0500

    fix

--- imports/import_base.py
+++ imports/import_base.py
@@ -1,8 +1,9 @@
 from common.feed_element import FeedElement
 
-class ImportBase:
-	def get_key(self):
-		raise NotImplementedError()
 
-	def get_elements(self, count):
-		raise NotImplementedError()
+class ImportBase:
+    def get_key(self):
+        raise NotImplementedError()
+
+    def get_elements(self, count):
+        raise NotImplementedError()
