6cc8d318aba598cb8ad717deffd200f1020db823
Kate Maltseva <kotoyama@list.ru>
2019-07-12 21:53:06 +0500

    TwitterImport implemented

--- imports/twitter_import.py
+++ imports/twitter_import.py
@@ -1,11 +1,12 @@
 import requests
 import urllib
-from urllib.request import urlopen
 
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from imports.import_base import ImportBase
 from common.feed_element import FeedElement
 from common.downloaded_image import DownloadedImage
+from common.linked_video import LinkedVideo
 
 
 class TwitterImport(ImportBase):
@@ -16,9 +17,8 @@ class TwitterImport(ImportBase):
         url = 'https://twitter.com/elonmusk'
         html = urlopen(url)
         soup = BeautifulSoup(html, 'html.parser')
+        tweets = soup.find_all('li', {'data-item-type': 'tweet'})
         result = []
-
-        tweets = soup.find_all('link', {'data-item-type': 'tweet'})
 
         for t in tweets:
             element = FeedElement()
@@ -26,9 +26,25 @@ class TwitterImport(ImportBase):
             element.body = t.find('p', {"class": 'tweet-text'}).get_text()
             element.date = t.find(
                 'a', {'class': 'tweet-timestamp'}).attrs['title']
+            tweet_id = t.attrs['data-item-id']
+
+            video = t.find(
+                'div', {'class': 'PlayableMedia--video'})
+
+            if video is None:
+                images = t.find_all(
+                    'div', {'class': 'AdaptiveMedia-photoContainer'})
+                for i in images:
+                    img_url = i.get('data-image-url')
+                    r = requests.get(img_url)
+                    element.images.append(DownloadedImage(r.content))
+            else:
+                video_address = 'https://twitter.com/i/status/{}'.format(
+                    tweet_id)
+                r = requests.get(video_address)
+                element.videos.append(LinkedVideo(r.content))
 
             result.append(element)
-            print(len(result))
-            # if len(result) >= count:
-            #     break
+            if len(result) >= count:
+                break
         return result
