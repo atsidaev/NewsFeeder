ab022b30dab78bf19ce4f59d292942245b789205
Alexander Tsidaev <a.tsidaev@gmail.com>
2019-07-16 19:55:46 +0500

    Retweets support

--- exports/stupid_html_export.py
+++ exports/stupid_html_export.py
@@ -5,25 +5,32 @@ from exports.export_base import ExportBase
 
 
 class StupidHtmlExport(ExportBase):
-    def __init__(self, filename):
-        self.filename = filename
+	def __init__(self, filename):
+		self.filename = filename
 
-        if not os.path.exists(filename):
-            with open(filename, "a+") as f:
-                f.write(
-                    '<head><meta http-equiv="refresh" content="10"></head><body>')
+		if not os.path.exists(filename):
+			with open(filename, "a+") as f:
+				f.write(
+					'<head><meta http-equiv="refresh" content="10"></head><body>')
 
-    def get_key(self):
-        return "stpd"
+	def get_key(self):
+		return "stpd"
 
-    def export(self, feed_element):
-        # Doing HTML like it was in 2000!
-        with open(self.filename, "a+", encoding='utf-8') as f:
-            f.write('<b>%s</b> <i>%s</i>' %
-                    (feed_element.date, feed_element.author))
-            f.write('<p>%s</p>' % feed_element.body)
-            for image in feed_element.images:
-                # hack: we assume that only DownloadedImage comes here and we know it's internal structure
-                f.write('<img src="data:image/jpeg;base64,%s" />' %
-                        base64.b64encode(image.file_contents).decode("utf-8"))
-            f.write("<hr />")
+	def write_feed_element(self, f, feed_element):
+		f.write('<b>%s</b> <i>%s</i>' % (feed_element.date, feed_element.author))
+		if feed_element.body:
+			f.write('<p>%s</p>' % feed_element.body)
+
+		for image in feed_element.images:
+			# hack: we assume that only DownloadedImage comes here and we know it's internal structure
+			f.write('<img src="data:image/jpeg;base64,%s" />' % base64.b64encode(image.file_contents).decode("utf-8"))
+			f.write("<hr />")
+		if feed_element.nested:
+			f.write("<div style='padding-left: 32px;'>Original message:<br />")
+			self.write_feed_element(f, feed_element.nested)
+			f.write("</div>")
+
+	def export(self, feed_element):
+		# Doing HTML like it was in 2000!
+		with open(self.filename, "a+", encoding='utf-8') as f:
+			self.write_feed_element(f, feed_element)
